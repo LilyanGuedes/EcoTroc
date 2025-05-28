1c313335faa079977fbff3783904ccd03384d44f
lilyanguedes <lilyangabyguedes@gmail.com>
2025-05-27 21:32:36 -0300

    feat: app modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USER as string,
+      password: process.env.DB_PASS as string,
+      database: process.env.DB_NAME as string,
+      entities: [__dirname + '/**/*.orm-entity.{ts,js}'],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
