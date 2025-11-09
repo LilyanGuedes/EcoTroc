f0e79a0046a1357016a32c813581cc71962d6357
lilyanguedes <lilyangabyguedes@gmail.com>
2025-11-09 12:03:31 -0300

    feat: add without-permission component and update recycler routes with role-based access control

--- frontend/src/app/routes/recycler.routes.ts
+++ frontend/src/app/routes/recycler.routes.ts
@@ -1,11 +1,33 @@
 import { Routes } from '@angular/router';
+import { roleGuard } from '../guards/role.guard';
+import { RoleReference } from '../entities/enums/role-reference.enum';
 
 export const recyclerRoutes: Routes = [
   {
     path: 'home',
+    canActivate: [roleGuard],
+    data: { roles: [RoleReference.RECYCLER] },
     loadComponent: () =>
       import('../modules/recycler/home-recycler/home-recycler.component').then(
         (c) => c.HomeRecyclerComponent
+      ),
+  },
+  {
+    path: 'points',
+    canActivate: [roleGuard],
+    data: { roles: [RoleReference.RECYCLER] },
+    loadComponent: () =>
+      import('../modules/recycler/points/points.component').then(
+        (c) => c.PointsComponent
+      ),
+  },
+  {
+    path: 'redeem',
+    canActivate: [roleGuard],
+    data: { roles: [RoleReference.RECYCLER] },
+    loadComponent: () =>
+      import('../modules/recycler/redeem-points/redeem-points.component').then(
+        (c) => c.RedeemPointsComponent
       ),
   },
 ];
