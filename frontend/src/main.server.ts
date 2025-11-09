c901229577d833783aaca37706f3873eb12c22e6
lilyanguedes <lilyangabyguedes@gmail.com>
2025-11-09 12:39:42 -0300

    feat: configure CORS settings in main.ts for improved security and access control

--- frontend/src/main.server.ts
+++ frontend/src/main.server.ts
@@ -1,4 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { renderApplication } from '@angular/platform-server';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
