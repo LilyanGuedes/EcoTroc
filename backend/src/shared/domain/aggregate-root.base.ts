a4b4e0077fc3a6f9f3e7756f4339aad34bc598d6
lilyanguedes <lilyangabyguedes@gmail.com>
2025-11-09 12:34:49 -0300

    feat: implement AggregateRoot class for managing domain events

---
+++ backend/src/shared/domain/aggregate-root.base.ts
@@ -0,0 +1,21 @@
+import { DomainEvent } from './domain-event.base';
+
+export abstract class AggregateRoot {
+  private _domainEvents: DomainEvent[] = [];
+
+  protected addDomainEvent(event: DomainEvent): void {
+    this._domainEvents.push(event);
+  }
+
+  public getDomainEvents(): DomainEvent[] {
+    return this._domainEvents.slice();
+  }
+
+  public clearDomainEvents(): void {
+    this._domainEvents = [];
+  }
+
+  public hasDomainEvents(): boolean {
+    return this._domainEvents.length > 0;
+  }
+}
