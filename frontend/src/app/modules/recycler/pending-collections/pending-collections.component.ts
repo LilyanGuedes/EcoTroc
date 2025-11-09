8cff98975cf4864e5d5ccf684dcb0d3e211fe17d
lilyanguedes <lilyangabyguedes@gmail.com>
2025-11-09 12:05:03 -0300

    feat: add PendingCollections component with HTML, CSS, and TypeScript files

---
+++ frontend/src/app/modules/recycler/pending-collections/pending-collections.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-collections',
+  imports: [],
+  templateUrl: './pending-collections.component.html',
+  styleUrl: './pending-collections.component.css'
+})
+export class PendingCollectionsComponent {
+
+}
